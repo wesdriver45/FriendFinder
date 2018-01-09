8516474d64b9e3d2b45a2ca493f8ea5f1bda06e4
wesdriver45 <wesdriver45@gmail.com>
2018-01-08 19:32:21 -0500

    home

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+//npm packages
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
-
+var fs = require("fs");
+//express server
 var app = express();
+//port
+var PORT = process.env.PORT || 3000;
+//data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//api/html routes
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+//listner
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
