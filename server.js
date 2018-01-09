349dc90dce7aa1956e84ab6feeab29e1f15f9de5
wesdriver45 <wesdriver45@gmail.com>
2018-01-08 19:50:27 -0500

    routes

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 //npm packages
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
 var fs = require("fs");
 //express server
 var app = express();
 //port
 var PORT = process.env.PORT || 3000;
+
 //data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 //api/html routes
 require("./routing/apiRoutes")(app);
 require("./routing/htmlRoutes")(app);
