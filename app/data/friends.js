f096aee8ff9cb9894282b77f8f9cc76537218c1f
wesdriver45 <wesdriver45@gmail.com>
2018-01-10 19:52:35 -0500

    survey

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,47 +1,48 @@
-var friends = [
+var friendsArray = [
 {
-	"name":"",
+	"name":"Amanda",
+	"photo":"https://www.pexels.com/photo/adult-beautiful-blonde-blur-324658/",
+	"scores":[3,2,1,5,4,1,2,5,3,4]
+},{
+	"name":"Bartholomew",
+	"photo": "https://www.pexels.com/photo/man-young-happy-smiling-91227/",
+	"scores":[1,2,4,2,4,3,5,3,2,4]
+},{
+	"name":"Claire",
+	"photo":"",
+	"scores":[3,3,2,3,4,3,5,3,4,1]
+},{
+	"name":"David",
+	"photo":"https://www.pexels.com/photo/adult-business-businessman-close-up-428339/",
+	"scores":[]
+},{
+	"name":"Elisa",
 	"photo":"",
 	"scores":[]
 },{
-	"name":"",
+	"name":"Fernando",
+	"photo":"https://www.pexels.com/photo/men-s-black-coat-with-white-polo-shirt-213117/",
+	"scores":[]
+},{
+	"name":"Gillian",
 	"photo":"",
 	"scores":[]
 },{
-	"name":"",
+	"name":"Howard",
 	"photo":"",
 	"scores":[]
 },{
-	"name":"",
+	"name":"Isabel",
 	"photo":"",
 	"scores":[]
 },{
-	"name":"",
+	"name":"Jasper",
 	"photo":"",
 	"scores":[]
 },{
-	"name":"",
-	"photo":"",
-	"scores":[]
-},{
-	"name":"",
-	"photo":"",
-	"scores":[]
-},{
-	"name":"",
-	"photo":"",
-	"scores":[]
-},{
-	"name":"",
-	"photo":"",
-	"scores":[]
-},{
-	"name":"",
-	"photo":"",
-	"scores":[]
-},{
-	"name":"",
+	"name":"Karina",
 	"photo":"",
 	"scores":[]
 },
-];
+
+module.exports = friendsArray;
