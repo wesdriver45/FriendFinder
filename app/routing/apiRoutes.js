d2ad0a9122b67324a9896c40768b69431eb658a2
wesdriver45 <wesdriver45@gmail.com>
2018-01-13 07:28:53 -0500

    psuedo code for routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,16 +8,28 @@ module.exports = function(app) {
 
 	//friend post 
 	app.post("/api/friends", function(req, res) {
+		//get new friend info
 		var newFriend = {
 			name: req.body.name,
 			photo: req.body.photo,
 			scores: []
 		};
 
+		//var matchFriends
+
+		//for loop through friends
+
+		//compare scores
+
+		//match friends
 
 
 
 
+
+
+
+	//add new friend
     friends.push(req.body);
 	});
 };
