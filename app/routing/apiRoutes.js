8f282c3d11eaeac8310c357811e98d7c919f025e
wesdriver45 <wesdriver45@gmail.com>
2018-01-13 07:08:58 -0500

    changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,11 +3,12 @@ var friendsData = require("../data/friends.js");
 //route
 module.exports = function(app) {
 	app.get("/api/friends", function(req, res) {
-		res.json(friends);
+		res.json(friendsData);
 	});
 
 	//friend post 
 	app.post("/api/friends", function(req, res) {
+		var newFriend = req.body
 
 		if (friends.length < 5) {
       friends.push(req.body);
