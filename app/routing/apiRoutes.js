cfea899b8da4ae263e0e2cbd3951506d4cf6ea55
wesdriver45 <wesdriver45@gmail.com>
2018-01-12 22:59:19 -0500

    route changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,5 +5,10 @@ module.exports = function(app) {
 	app.get("api/friends", function(req, res) {
 		res.json(friends);
 	});
+
+	//friend post 
+	app.post("api/friends", function(req, res) {
+		
+	})
 }
 
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
 	app.get("/survey", function(req, res) {
     	res.sendFile(path.join(__dirname, "../app/public/suyvey.html"));
     	//default to home
-    	app.get("*", function(req, res) {
+    	app.get("/", function(req, res) {
     	res.sendFile(path.join(__dirname, "../app/public/home.html"));
   	});
 
